c3979205fb8ed96d385e2b534e9dd4c5cd4eaa38
rehand <reinhard.handler@gmail.com>
2015-05-22 20:10:32 +0200

    removed unused import

--- src/at/fhj/ims/gae/poi/FHJ_GAE_POIServlet.java
+++ src/at/fhj/ims/gae/poi/FHJ_GAE_POIServlet.java
@@ -2,7 +2,6 @@ package at.fhj.ims.gae.poi;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
