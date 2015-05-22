fce2c0c394c3d193afa85ec014458c6efee21804
rehand <reinhard.handler@gmail.com>
2015-05-22 18:17:47 +0200

    added POI POJO

---
+++ src/at/fhj/ims/gae/poi/POI.java
@@ -0,0 +1,53 @@
+package at.fhj.ims.gae.poi;
+
+public class POI {
+	private String id;
+	private String name;
+	private String lat, lon;
+	private String creator;
+	private String description;
+	private String category;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLon() {
+		return lon;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
