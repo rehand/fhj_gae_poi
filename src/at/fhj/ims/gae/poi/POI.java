ad7418813e487dd19abcd8ba0e2f174185359a80
rehand <reinhard.handler@gmail.com>
2015-05-22 18:25:27 +0200

    added constructor

--- src/at/fhj/ims/gae/poi/POI.java
+++ src/at/fhj/ims/gae/poi/POI.java
@@ -7,46 +7,71 @@ public class POI {
 	private String creator;
 	private String description;
 	private String category;
-	
+
+	public POI(String id, String name, String lat, String lon, String creator,
+			String description, String category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.lat = lat;
+		this.lon = lon;
+		this.creator = creator;
+		this.description = description;
+		this.category = category;
+	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getLat() {
 		return lat;
 	}
+
 	public void setLat(String lat) {
 		this.lat = lat;
 	}
+
 	public String getLon() {
 		return lon;
 	}
+
 	public void setLon(String lon) {
 		this.lon = lon;
 	}
+
 	public String getCreator() {
 		return creator;
 	}
+
 	public void setCreator(String creator) {
 		this.creator = creator;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
